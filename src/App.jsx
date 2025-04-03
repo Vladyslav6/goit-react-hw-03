09d4661911d782e94705382cefe53eb89cfc4122
Vladyslav6 <vlad51284@gmail.com>
2025-04-04 02:31:27 +0300

    complite2step

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,23 @@ import ContactHomeWork from "./ContactsItem.json";
 
 function App() {
   const [ContactItem, ContactItems] = useState(ContactHomeWork);
+  const [FilterItem, SetFilter] = useState("");
   // const mapTest = ContactItem.map()
   // console.log(JSON.stringify(ContactItem))
   // const testMap = ContactItem.map((item, id) => console.log(item.id));
-
+  // const testSearch = ContactItem.filter((comment) =>
+  //   comment.name.toLowerCase().includes("rosi")
+  // );
+  const FilterContact = ContactItem.filter((item) =>
+    item.name.toLowerCase().includes(FilterItem.toLowerCase())
+  );
   return (
     <>
       <div>
         <h1>Phonebook</h1>
         <ContactFrom />
-        <SearchBox />
-        <ContactList StateContactItem={ContactItem} />
+        <SearchBox value={FilterItem} OnChangeFilter={SetFilter} />
+        <ContactList StateContactItem={FilterContact} />
       </div>
     </>
   );
--- src/Components/SearchBox/SearchBox.jsx
+++ src/Components/SearchBox/SearchBox.jsx
@@ -1,4 +1,15 @@
-const SearchBox = () => {
-  return <>Hi search</>;
+const SearchBox = ({ value, OnChangeFilter }) => {
+  return (
+    <>
+      <div>
+        <p>Find contact by name</p>
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => OnChangeFilter(e.target.value)}
+        ></input>
+      </div>
+    </>
+  );
 };
 export default SearchBox;
