428f1d2fe865ef26194d3e9cf29e457442edaf00
Vladyslav6 <vlad51284@gmail.com>
2025-04-05 02:25:46 +0300

    addForm

--- src/Components/ContactForm/ContactForm.jsx
+++ src/Components/ContactForm/ContactForm.jsx
@@ -1,4 +1,31 @@
+import { Field, Form, Formik } from "formik";
+import { number } from "yup";
+
 const ContactFrom = () => {
-  return <>Hi Contact</>;
+  const onSubmit = (value, options) => {
+    console.log(value);
+  };
+  const initialsValue = {
+    name: "",
+    number: "",
+  };
+  return (
+    <>
+      <Formik onSubmit={onSubmit} initialValues={initialsValue}>
+        <Form>
+          <label>
+            <span>Name</span>
+            <Field name="name"></Field>
+          </label>
+          <label>
+            <span>Number</span>
+            <Field name="number"></Field>
+          </label>
+
+          <button type="submit">Register</button>
+        </Form>
+      </Formik>
+    </>
+  );
 };
 export default ContactFrom;
